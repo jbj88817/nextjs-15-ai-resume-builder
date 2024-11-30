2a7b5e64a32f92122af5a1db835ee66d948ba47c
Bojie Jiang <jiangbojie@gmail.com>
2024-11-30 10:49:17 -0800

    - Sign in signup - Navbar - Theme - Prisma

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+}
